2b3ca832347317aeece6f0adc8dbe91503e1db4b
jontze <42588836+jontze@users.noreply.github.com>
2024-12-18 17:05:41 +0100

    feat(func_handler): Share logic to setup wasm functions between handler

--- wasm-function-runtime/src/handlers/function_handler.rs
+++ wasm-function-runtime/src/handlers/function_handler.rs
@@ -1,4 +1,5 @@
 use axum::{
+    body::Body,
     extract::{Path, Query, State},
     http::{HeaderMap, StatusCode},
     response::IntoResponse,
@@ -24,53 +25,12 @@ async fn handle_get_request(
     Query(query_map): Query<std::collections::HashMap<String, String>>,
     header_map: HeaderMap,
 ) -> impl IntoResponse {
-    // Check the cache registry if the wasm for the route is already precompiled
-    let registry_read = state.registry.read().await;
-    let (http_function, mut function_builder) = if let Some(precompiled_bytes) =
-        wasm_cache_service::extract_http_func(&registry_read, &path, "GET")
-    {
-        // If it is, execute the precompiled wasm
-        let mut http_function_builder = unsafe {
-            crate::component::http::FunctionHttpBuilder::deserialize(
-                &state.engine,
-                precompiled_bytes,
-            )
-        };
-        (http_function_builder.build().await, http_function_builder)
-    } else {
-        // If it is not, check the db if there is a function for the route
-        let (_, wasm_bytes) = if let Some((http_function, wasm_bytes)) =
-            function_service::find_http_func(&state.db, &path, "GET").await
-        {
-            (http_function, wasm_bytes)
+    let (function, mut function_builder) =
+        if let Some(func) = setup_function(state.clone(), &path, "POST").await {
+            func
         } else {
             return StatusCode::NOT_FOUND.into_response();
         };
-
-        // If there is, extract it from the filesystem and compile it.
-        let mut http_function_builder = {
-            crate::component::http::FunctionHttpBuilder::from_binary(&state.engine, &wasm_bytes)
-        };
-
-        println!("Read function from file");
-
-        // Then save the procompiled wasm to the cache registry to speed up future requests
-        drop(registry_read);
-        {
-            let precompiled_bytes = http_function_builder.serialize();
-            let mut write_registry = state.registry.write().await;
-            wasm_cache_service::cache_http_func(
-                &mut write_registry,
-                &path,
-                "GET",
-                &precompiled_bytes,
-            );
-        }
-
-        println!("Cached function");
-
-        (http_function_builder.build().await, http_function_builder)
-    };
 
     // Prepare the request to be passed to the function
     let query_params: Vec<bindings_function_http::QueryParam> = query_map
@@ -97,7 +57,7 @@ async fn handle_get_request(
     };
 
     // Execute the function
-    let function_response = http_function
+    let function_response = function
         .call_handle_request(&mut function_builder.store, &req)
         .await
         .expect("Failed to call function")
@@ -114,7 +74,68 @@ async fn handle_get_request(
 
 async fn handle_post_request(
     Path(path): Path<String>,
-    State(_): State<RuntimeStateRef>,
+    State(state): State<RuntimeStateRef>,
+    Query(query_map): Query<std::collections::HashMap<String, String>>,
+    header_map: HeaderMap,
+    body: Body,
 ) -> impl IntoResponse {
+    let (function, function_builder) =
+        if let Some(func) = setup_function(state.clone(), &path, "POST").await {
+            func
+        } else {
+            return StatusCode::NOT_FOUND.into_response();
+        };
+
     format!("Hello, {}!", path).into_response()
+}
+
+async fn setup_function(
+    state: RuntimeStateRef,
+    path: &str,
+    method: &str,
+) -> Option<(
+    bindings_function_http::FunctionHttp,
+    crate::component::http::FunctionHttpBuilder,
+)> {
+    let registry_read = state.registry.read().await;
+    if let Some(precompiled_bytes) =
+        wasm_cache_service::extract_http_func(&registry_read, &path, method)
+    {
+        // If it is, execute the precompiled wasm
+        let mut http_function_builder = unsafe {
+            crate::component::http::FunctionHttpBuilder::deserialize(
+                &state.engine,
+                precompiled_bytes,
+            )
+        };
+        drop(registry_read);
+        Some((http_function_builder.build().await, http_function_builder))
+    } else {
+        // If it is not, check the db if there is a function for the route
+        let (_, wasm_bytes) = if let Some((http_function, wasm_bytes)) =
+            function_service::find_http_func(&state.db, &path, method).await
+        {
+            (http_function, wasm_bytes)
+        } else {
+            return None;
+        };
+
+        // If there is, extract it from the filesystem and compile it.
+        let mut http_function_builder =
+            crate::component::http::FunctionHttpBuilder::from_binary(&state.engine, &wasm_bytes);
+
+        // Then save the procompiled wasm to the cache registry to speed up future requests
+        {
+            let precompiled_bytes = http_function_builder.serialize();
+            let mut write_registry = state.registry.write().await;
+            wasm_cache_service::cache_http_func(
+                &mut write_registry,
+                &path,
+                method,
+                &precompiled_bytes,
+            );
+        }
+
+        Some((http_function_builder.build().await, http_function_builder))
+    }
 }
