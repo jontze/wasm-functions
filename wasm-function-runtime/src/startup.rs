444c1ef8ba9b7ff50bd840b4791116f87887e7f4
Jontze <42588836+jontze@users.noreply.github.com>
2024-12-21 16:54:06 +0100

    feat(runtime): Add compression, tracing, trim and catch-panic middleware

--- wasm-function-runtime/src/startup.rs
+++ wasm-function-runtime/src/startup.rs
@@ -1,9 +1,31 @@
+use tower_http::ServiceBuilderExt;
 use tracing::info;
 
 use crate::{
     db,
     server_state::{RuntimeState, RuntimeStateRef},
 };
+
+#[derive(Default, Clone)]
+struct RequstIdGenerator {
+    counter: std::sync::Arc<std::sync::atomic::AtomicUsize>,
+}
+
+impl tower_http::request_id::MakeRequestId for RequstIdGenerator {
+    fn make_request_id<B>(
+        &mut self,
+        _: &http::Request<B>,
+    ) -> Option<tower_http::request_id::RequestId> {
+        let request_id = self
+            .counter
+            .fetch_add(1, std::sync::atomic::Ordering::SeqCst)
+            .to_string()
+            .parse()
+            .unwrap();
+
+        Some(tower_http::request_id::RequestId::new(request_id))
+    }
+}
 
 pub(crate) async fn run_server() {
     let db_pool = db::init_pool(
@@ -17,7 +39,15 @@ pub(crate) async fn run_server() {
 
     let app = crate::routes::create_routes()
         .with_state(runtime_state)
-        .layer(tower_http::trace::TraceLayer::new_for_http());
+        .layer(
+            tower::ServiceBuilder::new()
+                .trace_for_http()
+                .compression()
+                .set_x_request_id(RequstIdGenerator::default())
+                .propagate_x_request_id()
+                .catch_panic()
+                .trim_trailing_slash(),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
