d9d28f2751fc5307c82b75bfcfac8b9b9b9e29e9
Jontze <42588836+jontze@users.noreply.github.com>
2025-01-02 00:37:00 +0100

    tmp: Env example

--- examples/wasm-function-hello/src/lib.rs
+++ examples/wasm-function-hello/src/lib.rs
@@ -6,9 +6,25 @@ use bindings::Guest;
 struct Component;
 
 impl Guest for Component {
-    /// Say hello!
-    fn hello_world() -> String {
-        "Hello, World!".to_string()
+    fn handle_request(_req: bindings::Request) -> Result<bindings::Response, ()> {
+        let mut response = bindings::Response {
+            status_code: 200,
+            headers: vec![bindings::Header {
+                name: "Content-Type".to_string(),
+                value: "text/plain".to_string(),
+            }],
+            body: vec![],
+        };
+
+        let envs: Vec<(String, String)> = std::env::vars().collect();
+
+        let key_value_string = envs
+            .iter()
+            .map(|(k, v)| format!("{}: {}\n", k, v))
+            .collect::<String>();
+
+        response.body = key_value_string.as_bytes().to_vec();
+        Ok(response)
     }
 }
 
